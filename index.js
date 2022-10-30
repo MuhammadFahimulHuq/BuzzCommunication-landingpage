ecfe40427a56a5c8f9f109967a971bf489e0e409
fahim1996112016 <fahimul.huq85@gmail.com>
2022-10-30 16:23:18 +0600

    second commit

--- index.js
+++ index.js
@@ -1,15 +1,23 @@
 const menu_btn = document.querySelector('.hamburger');
 const mobile_menu = document.querySelector('.mobile-nav')
 const title = document.querySelector('.title')
+const menu_link = document.querySelectorAll('.menuItem')
 menu_btn.addEventListener('click',function(){
     menu_btn.classList.toggle('is-active');
     mobile_menu.classList.toggle('is-active');
     title.classList.toggle('title-is-active');
+   
 })
-
-gsap.from('.middle-wrapper',{opactiy:0,duration:1.5,y:-50,ease:'Power2.easeInOut'})
-gsap.from('.top-wrapper',{opactiy:0,duration:1.5,x:-50})
-gsap.from('.last-wrapper',{opactiy:0,duration:1.5,x:50})
+menu_link.forEach(function(elem){
+    elem.addEventListener('click',function(){
+        menu_btn.classList.remove('is-active');
+        mobile_menu.classList.remove('is-active');
+        title.classList.remove('title-is-active');
+    })
+})
+gsap.from('.middle-wrapper',{cssRule: {opacity: 0},duration:1.5,y:-50,ease:'power3.inOut'})
+gsap.from('.top-wrapper',{cssRule: {opacity: 0},duration:1.5,x:-20})
+gsap.from('.last-wrapper',{cssRule: {opacity: 0},duration:1.5,x:50})
 
 
 gsap.to('.image-1',{
